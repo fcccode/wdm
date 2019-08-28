d19cb5d4de7468e4248e121164993c0971e2ff12
steward-fu <steward.fu@gmail.com>
2019-08-28 06:09:09 -0400

    init

--- asm/startio/sys/main.asm
+++ asm/startio/sys/main.asm
@@ -1,14 +1,3 @@
-;//========================================================================================================
-;//  Basically, all of files downloaded from my website can be modified or redistributed for any purpose.
-;//  It is my honor to share my interesting to everybody.
-;//  If you find any illeage content out from my website, please contact me firstly.
-;//  I will remove all of the illeage parts.
-;//  Thanks :)
-;//  
-;//  Steward Fu
-;//  g9313716@yuntech.edu.tw
-;//  https://steward-fu.github.io/website/index.htm
-;//========================================================================================================*/
 .386p
 .model flat, stdcall
 option casemap:none
@@ -23,91 +12,90 @@ includelib c:\masm32\lib\wxp\i386\ntoskrnl.lib
 public DriverEntry
 
 OurDeviceExtension struct
-	pNextDev        PDEVICE_OBJECT ?
-	szBuffer        BYTE 1024 dup(?)
-  dwBufferLength  DWORD ?
+  pNextDev PDEVICE_OBJECT ?
+  szBuffer BYTE 255 dup(?)
 OurDeviceExtension ends
 
 .const
-DEV_NAME word "\","D","e","v","i","c","e","\","f","i","r","s","t","S","t","a","r","t","I","O",0
-SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","f","i","r","s","t","S","t","a","r","t","I","O",0
+DEV_NAME word "\","D","e","v","i","c","e","\","M","y","D","r","i","v","e","r",0
+SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","M","y","D","r","i","v","e","r",0
 
 .code
-;//*** startio routine
 StartIo proc uses ebx pOurDevice:PDEVICE_OBJECT, pIrp:PIRP
-	local pdx:PTR OurDeviceExtension
-	local dwLen:DWORD
-	local pBuf:DWORD
-	
-	mov eax, pOurDevice
-	push (DEVICE_OBJECT PTR [eax]).DeviceExtension
-	pop pdx
-	
-	mov eax, pIrp
-	push (_IRP PTR [eax]).AssociatedIrp.SystemBuffer
-	pop pBuf
-	
-	IoGetCurrentIrpStackLocation pIrp
-	Push (IO_STACK_LOCATION PTR [eax]).Parameters.Write._Length
-	pop dwLen
-	movzx ebx, (IO_STACK_LOCATION PTR [eax]).MajorFunction
-	.if ebx == IRP_MJ_WRITE
-		invoke DbgPrint, $CTA0("StartIo: IRP_MJ_WRITE")
-		mov eax, pdx
-		push dwLen
-		pop (OurDeviceExtension PTR [eax]).dwBufferLength
-		invoke memcpy, addr (OurDeviceExtension PTR [eax]).szBuffer, pBuf, dwLen
-		invoke DbgPrint, $CTA0("len: %d"), dwLen
-	.elseif ebx == IRP_MJ_READ
-		invoke DbgPrint, $CTA0("StartIo: IRP_MJ_READ")
-		mov eax, pdx
-		push (OurDeviceExtension PTR [eax]).dwBufferLength
-		pop dwLen
-		invoke memcpy, pBuf, addr (OurDeviceExtension PTR [eax]).szBuffer, dwLen
-		invoke DbgPrint, $CTA0("buf: %s"), pBuf
-	.endif
-	
-	mov eax, pIrp
-	mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
-	push dwLen
-	pop (_IRP PTR [eax]).IoStatus.Information
-	fastcall IofCompleteRequest, pIrp, IO_NO_INCREMENT
-	invoke IoStartNextPacket, pOurDevice, FALSE
-	mov eax, STATUS_SUCCESS
-	ret
+  local pdx:PTR OurDeviceExtension
+  local dwLen:DWORD
+  local pBuf:DWORD
+  
+  mov eax, pOurDevice
+  push (DEVICE_OBJECT PTR [eax]).DeviceExtension
+  pop pdx
+  
+  mov eax, pIrp
+  push (_IRP PTR [eax]).AssociatedIrp.SystemBuffer
+  pop pBuf
+  
+  IoGetCurrentIrpStackLocation pIrp
+  movzx ebx, (IO_STACK_LOCATION PTR [eax]).MajorFunction
+  .if ebx == IRP_MJ_WRITE
+    invoke DbgPrint, $CTA0("StartIo, IRP_MJ_WRITE")
+    IoGetCurrentIrpStackLocation pIrp
+    Push (IO_STACK_LOCATION PTR [eax]).Parameters.Write._Length
+    pop dwLen
+    
+    mov eax, pdx
+    invoke memcpy, addr (OurDeviceExtension PTR [eax]).szBuffer, pBuf, dwLen
+    invoke DbgPrint, $CTA0("Buffer: %s, Length: %d"), pBuf, dwLen
+  .elseif ebx == IRP_MJ_READ
+    invoke DbgPrint, $CTA0("StartIo, IRP_MJ_READ")
+    mov eax, pdx
+    invoke strlen, addr (OurDeviceExtension PTR [eax]).szBuffer
+    inc eax
+    push eax
+    pop dwLen
+    
+    mov eax, pdx
+    invoke memcpy, pBuf, addr (OurDeviceExtension PTR [eax]).szBuffer, dwLen
+  .endif
+  
+  mov eax, pIrp
+  mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
+  push dwLen
+  pop (_IRP PTR [eax]).IoStatus.Information
+  fastcall IofCompleteRequest, pIrp, IO_NO_INCREMENT
+  invoke IoStartNextPacket, pOurDevice, FALSE
+  mov eax, STATUS_SUCCESS
+  ret
 StartIo endp
 
-;//*** process file irp
 IrpFile proc uses ebx pOurDevice:PDEVICE_OBJECT, pIrp:PIRP
   IoGetCurrentIrpStackLocation pIrp
   movzx ebx, (IO_STACK_LOCATION PTR [eax]).MajorFunction
   .if (ebx == IRP_MJ_CREATE) || (ebx == IRP_MJ_CLOSE) 
     .if (ebx == IRP_MJ_CREATE)
-      invoke DbgPrint, $CTA0("IrpFile: IRP_MJ_CREATE")
+      invoke DbgPrint, $CTA0("IRP_MJ_CREATE")
     .else
-      invoke DbgPrint, $CTA0("IrpFile: IRP_MJ_CLOSE")
+      invoke DbgPrint, $CTA0("IRP_MJ_CLOSE")
     .endif
-		mov eax, pIrp
-		and (_IRP PTR [eax]).IoStatus.Information, 0
-		mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
-		fastcall IofCompleteRequest, pIrp, IO_NO_INCREMENT
+    mov eax, pIrp
+    and (_IRP PTR [eax]).IoStatus.Information, 0
+    mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
+    fastcall IofCompleteRequest, pIrp, IO_NO_INCREMENT
     mov eax, STATUS_SUCCESS
   .elseif (ebx == IRP_MJ_READ) || (ebx == IRP_MJ_WRITE)
-		IoGetCurrentIrpStackLocation pIrp
+    IoGetCurrentIrpStackLocation pIrp
     .if (ebx == IRP_MJ_READ)
-      invoke DbgPrint, $CTA0("IrpFile: IRP_MJ_READ")
+      invoke DbgPrint, $CTA0("IrpFile, IRP_MJ_READ")
     .else
-      invoke DbgPrint, $CTA0("IrpFile: IRP_MJ_WRITE")
+      invoke DbgPrint, $CTA0("IrpFile, IRP_MJ_WRITE")
     .endif
-		IoMarkIrpPending pIrp
-		invoke IoStartPacket, pOurDevice, pIrp, 0, NULL
-		mov eax, STATUS_PENDING
+    IoMarkIrpPending pIrp
+    invoke IoStartPacket, pOurDevice, pIrp, 0, NULL
+    mov eax, STATUS_PENDING
   .endif
   ret
 IrpFile endp
 
-;//*** process pnp irp
-IrpPnP proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
+IrpPnp proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   local pdx:PTR OurDeviceExtension
   local szSymName:UNICODE_STRING
 
@@ -135,18 +123,15 @@ IrpPnP proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   mov eax, pdx
   invoke IoCallDriver, (OurDeviceExtension PTR [eax]).pNextDev, pIrp
   ret
-IrpPnP endp
+IrpPnp endp
 
-;//*** system will vist this routine when it needs to add new device
 AddDevice proc pOurDriver:PDRIVER_OBJECT, pPhyDevice:PDEVICE_OBJECT
   local pOurDevice:PDEVICE_OBJECT
   local suDevName:UNICODE_STRING
   local szSymName:UNICODE_STRING
 
-  invoke DbgPrint, $CTA0("MASM32 WDM driver tutorial for StartIO")
   invoke RtlInitUnicodeString, addr suDevName, offset DEV_NAME
   invoke RtlInitUnicodeString, addr szSymName, offset SYM_NAME
-
   invoke IoCreateDevice, pOurDriver, sizeof OurDeviceExtension, addr suDevName, FILE_DEVICE_UNKNOWN, 0, FALSE, addr pOurDevice
   .if eax == STATUS_SUCCESS
     invoke IoAttachDeviceToDeviceStack, pOurDevice, pPhyDevice
@@ -167,27 +152,25 @@ AddDevice proc pOurDriver:PDRIVER_OBJECT, pPhyDevice:PDEVICE_OBJECT
   .endif
   ret
 AddDevice endp
-  
-;//*** it is time to unload our driver
+
 Unload proc pOurDriver:PDRIVER_OBJECT
-	xor eax, eax
-	ret
+  xor eax, eax
+  ret
 Unload endp
 
-;//*** driver entry
 DriverEntry proc pOurDriver:PDRIVER_OBJECT, pOurRegistry:PUNICODE_STRING
-	mov eax, pOurDriver
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_PNP    * (sizeof PVOID)], offset IrpPnP
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CREATE * (sizeof PVOID)], offset IrpFile
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CLOSE  * (sizeof PVOID)], offset IrpFile
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_WRITE  * (sizeof PVOID)], offset IrpFile
+  mov eax, pOurDriver
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_PNP    * (sizeof PVOID)], offset IrpPnp
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CREATE * (sizeof PVOID)], offset IrpFile
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CLOSE  * (sizeof PVOID)], offset IrpFile
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_WRITE  * (sizeof PVOID)], offset IrpFile
   mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_READ   * (sizeof PVOID)], offset IrpFile
   mov (DRIVER_OBJECT PTR [eax]).DriverStartIo, offset StartIo
-	mov (DRIVER_OBJECT PTR [eax]).DriverUnload, offset Unload
-	mov eax, (DRIVER_OBJECT PTR [eax]).DriverExtension
-	mov (DRIVER_EXTENSION PTR [eax]).AddDevice, AddDevice
-	mov eax, STATUS_SUCCESS
-	ret
+  mov (DRIVER_OBJECT PTR [eax]).DriverUnload, offset Unload
+  mov eax, (DRIVER_OBJECT PTR [eax]).DriverExtension
+  mov (DRIVER_EXTENSION PTR [eax]).AddDevice, AddDevice
+  mov eax, STATUS_SUCCESS
+  ret
 DriverEntry endp
 end DriverEntry
 .end
--- cpp/startio/sys/main.c
+++ cpp/startio/sys/main.c
@@ -1,23 +1,11 @@
-/*========================================================================================================
-  Basically, all of files downloaded from my website can be modified or redistributed for any purpose.
-  It is my honor to share my interesting to everybody.
-  If you find any illeage content out from my website, please contact me firstly.
-  I will remove all of the illeage parts.
-  Thanks :)
-   
-  Steward Fu
-  g9313716@yuntech.edu.tw
-  https://steward-fu.github.io/website/index.htm
-========================================================================================================*/
 #include <wdm.h>
 
-#define DEV_NAME  L"\\Device\\firstStartIO"
-#define SYM_NAME  L"\\DosDevices\\firstStartIO"
-#define MSG       "WDM driver tutorial for StartIO"
+#define DEV_NAME L"\\Device\\MyDriver"
+#define SYM_NAME L"\\DosDevices\\MyDriver"
 
-PDEVICE_OBJECT gNextDevice=NULL;
-char szBuffer[1024]={0};
+char szBuffer[255]={0};
 ULONG dwBufferLength=0;
+PDEVICE_OBJECT pNextDevice=NULL;
 
 VOID StartIo(struct _DEVICE_OBJECT *pOurDevice, struct _IRP *pIrp)
 {
@@ -25,15 +13,15 @@ VOID StartIo(struct _DEVICE_OBJECT *pOurDevice, struct _IRP *pIrp)
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
   switch(psk->MajorFunction){
   case IRP_MJ_READ:
-    dwLen = psk->Parameters.Read.Length;
+    dwLen = strlen(szBuffer)+1;
     strcpy(pIrp->AssociatedIrp.SystemBuffer, szBuffer);
-    DbgPrint("StartIo, IRP_MJ_READ\n");
+    DbgPrint("StartIo, IRP_MJ_READ");
     break;
   case IRP_MJ_WRITE:
     dwLen = psk->Parameters.Write.Length;
     strcpy(szBuffer, pIrp->AssociatedIrp.SystemBuffer);
-    DbgPrint("StartIo, IRP_MJ_WRITE\n");
-    DbgPrint("StartIo, Buffer: %s\n", szBuffer);
+    DbgPrint("StartIo, IRP_MJ_WRITE");
+    DbgPrint("Buffer: %s, Length: %d", szBuffer, dwLen);
     break;
   }
   pIrp->IoStatus.Status = STATUS_SUCCESS;
@@ -42,31 +30,27 @@ VOID StartIo(struct _DEVICE_OBJECT *pOurDevice, struct _IRP *pIrp)
   IoStartNextPacket(pOurDevice, FALSE);
 }
 
-//*** system will vist this routine when it needs to add new device
 NTSTATUS AddDevice(PDRIVER_OBJECT pOurDriver, PDEVICE_OBJECT pPhyDevice)
 {
   PDEVICE_OBJECT pOurDevice=NULL;
   UNICODE_STRING usDeviceName;
   UNICODE_STRING usSymboName;
 
-  DbgPrint(MSG);
   RtlInitUnicodeString(&usDeviceName, DEV_NAME);
   IoCreateDevice(pOurDriver, 0, &usDeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &pOurDevice);
   RtlInitUnicodeString(&usSymboName, SYM_NAME);
   IoCreateSymbolicLink(&usSymboName, &usDeviceName);
-  gNextDevice = IoAttachDeviceToDeviceStack(pOurDevice, pPhyDevice);
+  pNextDevice = IoAttachDeviceToDeviceStack(pOurDevice, pPhyDevice);
   pOurDevice->Flags&= ~DO_DEVICE_INITIALIZING;
   pOurDevice->Flags|= DO_BUFFERED_IO;
   return STATUS_SUCCESS;
 }
-  
-//*** it is time to unload our driver
+
 void Unload(PDRIVER_OBJECT pOurDriver)
 {
   pOurDriver = pOurDriver;
 }
 
-//*** process pnp irp
 NTSTATUS IrpPnp(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
 {
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
@@ -75,41 +59,39 @@ NTSTATUS IrpPnp(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
   if(psk->MinorFunction == IRP_MN_REMOVE_DEVICE){
     RtlInitUnicodeString(&usSymboName, SYM_NAME);
     IoDeleteSymbolicLink(&usSymboName);
-    IoDetachDevice(gNextDevice);
+    IoDetachDevice(pNextDevice);
     IoDeleteDevice(pOurDevice);
   }
   IoSkipCurrentIrpStackLocation(pIrp);
-  return IoCallDriver(gNextDevice, pIrp);
+  return IoCallDriver(pNextDevice, pIrp);
 }
 
-//*** process file irp
 NTSTATUS IrpFile(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
 {
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
 
   switch(psk->MajorFunction){
   case IRP_MJ_CREATE:
-    DbgPrint("IrpFile, IRP_MJ_CREATE\n");
+    DbgPrint("IRP_MJ_CREATE");
     break;
   case IRP_MJ_WRITE:
-    DbgPrint("IrpFile, IRP_MJ_WRITE\n");
+    DbgPrint("IrpFile, IRP_MJ_WRITE");
     IoMarkIrpPending(pIrp);
     IoStartPacket(pOurDevice, pIrp, 0, NULL);
     return STATUS_PENDING;
   case IRP_MJ_READ:
-    DbgPrint("IrpFile, IRP_MJ_READ\n");
+    DbgPrint("IrpFile, IRP_MJ_READ");
     IoMarkIrpPending(pIrp);
     IoStartPacket(pOurDevice, pIrp, 0, NULL);
     return STATUS_PENDING;
   case IRP_MJ_CLOSE:
-    DbgPrint("IrpFile, IRP_MJ_CLOSE\n");
+    DbgPrint("IRP_MJ_CLOSE");
     break;
   }
   IoCompleteRequest(pIrp, IO_NO_INCREMENT);
   return STATUS_SUCCESS;
 }
-  
-//*** driver entry
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT pOurDriver, PUNICODE_STRING pOurRegistry)
 {
   pOurDriver->MajorFunction[IRP_MJ_PNP] = IrpPnp;
