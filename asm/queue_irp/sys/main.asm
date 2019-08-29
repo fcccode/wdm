dd8a8dd6ce4c3cb31367d01db1204ed4a82506c6
steward-fu <steward.fu@gmail.com>
2019-08-29 02:44:16 -0400

    init

--- asm/queue_irp/sys/main.asm
+++ asm/queue_irp/sys/main.asm
@@ -1,14 +1,3 @@
-;//========================================================================================================
-;//  Basically, all of files downloaded from my website can be modified or redistributed for any purpose.
-;//  It is my honor to share my interesting to everybody.
-;//  If you find any illeage content out from my website, please contact me firstly.
-;//  I will remove all of the illeage parts.
-;//  Thanks :)
-;//  
-;//  Steward Fu
-;//  g9313716@yuntech.edu.tw
-;//  https://steward-fu.github.io/website/index.htm
-;//========================================================================================================*/
 .386p
 .model flat, stdcall
 option casemap:none
@@ -23,50 +12,50 @@ includelib c:\masm32\lib\wxp\i386\ntoskrnl.lib
 public DriverEntry
 
 OurDeviceExtension struct
-	pNextDev    PDEVICE_OBJECT ?
-	stQueue         LIST_ENTRY      <>
-  stTimeDPC       KDPC            <>
-  stTime          KTIMER          <>
+  pNextDev  PDEVICE_OBJECT ?
+  stQueue   LIST_ENTRY <>
+  stDPC     KDPC <>
+  stTime    KTIMER <>
 OurDeviceExtension ends
 
-IOCTL_QUEUE_IT    equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_BUFFERED,    FILE_ANY_ACCESS)
-IOCTL_PROCESS_IT  equ CTL_CODE(FILE_DEVICE_UNKNOWN, 801h, METHOD_BUFFERED,    FILE_ANY_ACCESS)
+IOCTL_QUEUE   equ CTL_CODE(FILE_DEVICE_UNKNOWN, 800h, METHOD_BUFFERED, FILE_ANY_ACCESS)
+IOCTL_PROCESS equ CTL_CODE(FILE_DEVICE_UNKNOWN, 801h, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 .const
-DEV_NAME word "\","D","e","v","i","c","e","\","f","i","r","s","t","Q","u","e","u","e",0
-SYM_NAME word "\","D","o","s","D","e","v","i","c","e","s","\","f","i","r","s","t","Q","u","e","u","e",0
+DEV_NAME    word "\","D","e","v","i","c","e","\","M","y","D","r","i","v","e","r",0
+SYM_NAME    word "\","D","o","s","D","e","v","i","c","e","s","\","M","y","D","r","i","v","e","r",0
+MSG_QUEUE   byte "IOCTL_QUEUE",0
+MSG_PROCESS byte "IOCTL_PROCESS",0
 
 .code
-;//*** timer routine
 OnTimer proc pDpc:PKDPC, pContext:PVOID, pArg1:PVOID, PArg2:PVOID
-	mov eax, pContext
-	mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
-	lea eax, (OurDeviceExtension PTR [eax]).stQueue
-	IsListEmpty eax
-	.if eax == TRUE
-		mov eax, pContext
-		mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
-		invoke KeCancelTimer, addr (OurDeviceExtension PTR [eax]).stTime
-		invoke DbgPrint, $CTA0("OnTimer: Process successfully")
-	.else
-		mov eax, pContext
-		mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
-		lea eax, (OurDeviceExtension PTR [eax]).stQueue
-		RemoveHeadList eax
-		;// CONTAINING_RECORD 
-		sub eax, _IRP.Tail.Overlay.ListEntry
-		;// complete queued Irp
-		mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
-		push 0
-		pop (_IRP PTR [eax]).IoStatus.Information
-		fastcall IofCompleteRequest, eax, IO_NO_INCREMENT
-		mov eax, STATUS_SUCCESS
-		invoke DbgPrint, $CTA0("OnTimer: Processing Irp")
-	.endif
-	ret
+  mov eax, pContext
+  mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
+  lea eax, (OurDeviceExtension PTR [eax]).stQueue
+  IsListEmpty eax
+  .if eax == TRUE
+    mov eax, pContext
+    mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
+    invoke KeCancelTimer, addr (OurDeviceExtension PTR [eax]).stTime
+    invoke DbgPrint, $CTA0("Finish")
+  .else
+    mov eax, pContext
+    mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
+    lea eax, (OurDeviceExtension PTR [eax]).stQueue
+    RemoveHeadList eax
+    
+    ;// CONTAINING_RECORD 
+    sub eax, _IRP.Tail.Overlay.ListEntry
+    mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
+    push 0
+    pop (_IRP PTR [eax]).IoStatus.Information
+    fastcall IofCompleteRequest, eax, IO_NO_INCREMENT
+    mov eax, STATUS_SUCCESS
+    invoke DbgPrint, $CTA0("Complete Irp")
+  .endif
+  ret
 OnTimer endp
 
-;//*** process file irp
 IrpOpenClose proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   IoGetCurrentIrpStackLocation pIrp
   movzx ebx, (IO_STACK_LOCATION PTR [eax]).MajorFunction
@@ -84,14 +73,9 @@ IrpOpenClose proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   ret
 IrpOpenClose endp
 
-;//*** process ioctl irp
 IrpIOCTL proc uses ebx ecx pOurDevice:PDEVICE_OBJECT, pIrp:PIRP
-	local dwLen: DWORD
-	local pdx:PTR Dev_Ext
-	local stTimePeriod:LARGE_INTEGER
-
-  push 0
-  pop dwLen
+  local pdx:PTR Dev_Ext
+  local stTimePeriod:LARGE_INTEGER
 
   mov eax, pOurDevice
   push (DEVICE_OBJECT PTR [eax]).DeviceExtension
@@ -99,36 +83,36 @@ IrpIOCTL proc uses ebx ecx pOurDevice:PDEVICE_OBJECT, pIrp:PIRP
 
   IoGetCurrentIrpStackLocation pIrp
   mov eax, (IO_STACK_LOCATION PTR [eax]).Parameters.DeviceIoControl.IoControlCode
-  .if eax == IOCTL_QUEUE_IT
-    invoke DbgPrint, $CTA0("_IOCTL_QUEUE_IT")
+  .if eax == IOCTL_QUEUE
+    invoke DbgPrint, offset MSG_QUEUE
+
+    mov ebx, pdx
+    lea ebx, (OurDeviceExtension PTR [ebx]).stQueue
+    mov ecx, pIrp
+    lea ecx, (_IRP PTR [ecx]).Tail.Overlay.ListEntry
+    InsertHeadList ebx, ecx
+    IoMarkIrpPending pIrp
+    mov eax, STATUS_PENDING
+    ret
+  .elseif eax == IOCTL_PROCESS
+    invoke DbgPrint, offset MSG_PROCESS
     
-		mov ebx, pdx
-		lea ebx, (OurDeviceExtension PTR [ebx]).stQueue
-		mov ecx, pIrp
-		lea ecx, (_IRP PTR [ecx]).Tail.Overlay.ListEntry
-		InsertHeadList ebx, ecx
-		IoMarkIrpPending pIrp
-		mov eax, STATUS_PENDING
-		ret
-  .elseif eax == IOCTL_PROCESS_IT
-    invoke DbgPrint, $CTA0("_IOCTL_PROCESS_IT")
-		or stTimePeriod.HighPart, -1
-		mov stTimePeriod.LowPart, -10000000
-		mov ebx, pdx
-		invoke KeSetTimerEx, addr (OurDeviceExtension PTR [ebx]).stTime, stTimePeriod.LowPart, stTimePeriod.HighPart, 1000, addr (OurDeviceExtension PTR [ebx]).stTimeDPC
+    or stTimePeriod.HighPart, -1
+    mov stTimePeriod.LowPart, -10000000
+    mov ebx, pdx
+    invoke KeSetTimerEx, addr (OurDeviceExtension PTR [ebx]).stTime, stTimePeriod.LowPart, stTimePeriod.HighPart, 10, addr (OurDeviceExtension PTR [ebx]).stDPC
   .endif
 
   mov eax, pIrp
   mov (_IRP PTR [eax]).IoStatus.Status, STATUS_SUCCESS
-  push dwLen
+  push 0
   pop (_IRP PTR [eax]).IoStatus.Information 
   fastcall IofCompleteRequest, pIrp, IO_NO_INCREMENT
   mov eax, STATUS_SUCCESS
   ret
 IrpIOCTL endp
 
-;//*** process pnp irp
-IrpPnP proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
+IrpPnp proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   local pdx:PTR OurDeviceExtension
   local szSymName:UNICODE_STRING
 
@@ -156,18 +140,15 @@ IrpPnP proc uses ebx pDevObj:PDEVICE_OBJECT, pIrp:PIRP
   mov eax, pdx
   invoke IoCallDriver, (OurDeviceExtension PTR [eax]).pNextDev, pIrp
   ret
-IrpPnP endp
+IrpPnp endp
 
-;//*** system will vist this routine when it needs to add new device
 AddDevice proc uses ebx pOurDriver:PDRIVER_OBJECT, pPhyDevice:PDEVICE_OBJECT
   local pOurDevice:PDEVICE_OBJECT
   local suDevName:UNICODE_STRING
   local szSymName:UNICODE_STRING
 
-  invoke DbgPrint, $CTA0("MASM32 WDM driver tutorial for Queue")
   invoke RtlInitUnicodeString, addr suDevName, offset DEV_NAME
   invoke RtlInitUnicodeString, addr szSymName, offset SYM_NAME
-
   invoke IoCreateDevice, pOurDriver, sizeof OurDeviceExtension, addr suDevName, FILE_DEVICE_UNKNOWN, 0, FALSE, addr pOurDevice
   .if eax == STATUS_SUCCESS
     invoke IoAttachDeviceToDeviceStack, pOurDevice, pPhyDevice
@@ -181,22 +162,19 @@ AddDevice proc uses ebx pOurDriver:PDRIVER_OBJECT, pPhyDevice:PDEVICE_OBJECT
       or (DEVICE_OBJECT PTR [eax]).Flags, DO_BUFFERED_IO
       and (DEVICE_OBJECT PTR [eax]).Flags, not DO_DEVICE_INITIALIZING
       mov eax, STATUS_SUCCESS
-      
-      ;// initialize queue
-			mov ebx, pOurDevice
-			mov ebx, (DEVICE_OBJECT PTR [ebx]).DeviceExtension
-			lea ebx, (OurDeviceExtension PTR [ebx]).stQueue
-			InitializeListHead ebx
-			
-			;// initialize timer
-			mov eax, pOurDevice
-			mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
-			invoke KeInitializeTimer, addr (OurDeviceExtension ptr [eax]).stTime
-			
-			;// initialize dpc
-			mov eax, pOurDevice
-			mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
-			invoke KeInitializeDpc, addr (OurDeviceExtension ptr [eax]).stTimeDPC, offset OnTimer, pOurDevice
+
+      mov ebx, pOurDevice
+      mov ebx, (DEVICE_OBJECT PTR [ebx]).DeviceExtension
+      lea ebx, (OurDeviceExtension PTR [ebx]).stQueue
+      InitializeListHead ebx
+
+      mov eax, pOurDevice
+      mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
+      invoke KeInitializeTimer, addr (OurDeviceExtension ptr [eax]).stTime
+
+      mov eax, pOurDevice
+      mov eax, (DEVICE_OBJECT PTR [eax]).DeviceExtension
+      invoke KeInitializeDpc, addr (OurDeviceExtension ptr [eax]).stDPC, offset OnTimer, pOurDevice
     .else
       mov eax, STATUS_UNSUCCESSFUL
     .endif      
@@ -204,25 +182,23 @@ AddDevice proc uses ebx pOurDriver:PDRIVER_OBJECT, pPhyDevice:PDEVICE_OBJECT
   .endif
   ret
 AddDevice endp
-  
-;//*** it is time to unload our driver
+
 Unload proc pOurDriver:PDRIVER_OBJECT
-	xor eax, eax
-	ret
+  xor eax, eax
+  ret
 Unload endp
 
-;//*** driver entry
 DriverEntry proc pOurDriver:PDRIVER_OBJECT, pOurRegistry:PUNICODE_STRING
-	mov eax, pOurDriver
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_PNP    * (sizeof PVOID)], offset IrpPnP
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CREATE * (sizeof PVOID)], offset IrpOpenClose
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CLOSE  * (sizeof PVOID)], offset IrpOpenClose
-	mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_DEVICE_CONTROL * (sizeof PVOID)], offset IrpIOCTL
-	mov (DRIVER_OBJECT PTR [eax]).DriverUnload, offset Unload
-	mov eax, (DRIVER_OBJECT PTR [eax]).DriverExtension
-	mov (DRIVER_EXTENSION PTR [eax]).AddDevice, AddDevice
-	mov eax, STATUS_SUCCESS
-	ret
+  mov eax, pOurDriver
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_PNP    * (sizeof PVOID)], offset IrpPnp
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CREATE * (sizeof PVOID)], offset IrpOpenClose
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_CLOSE  * (sizeof PVOID)], offset IrpOpenClose
+  mov (DRIVER_OBJECT PTR [eax]).MajorFunction[IRP_MJ_DEVICE_CONTROL * (sizeof PVOID)], offset IrpIOCTL
+  mov (DRIVER_OBJECT PTR [eax]).DriverUnload, offset Unload
+  mov eax, (DRIVER_OBJECT PTR [eax]).DriverExtension
+  mov (DRIVER_EXTENSION PTR [eax]).AddDevice, AddDevice
+  mov eax, STATUS_SUCCESS
+  ret
 DriverEntry endp
 end DriverEntry
 .end
--- cpp/queue_irp/sys/main.c
+++ cpp/queue_irp/sys/main.c
@@ -1,36 +1,23 @@
-/*========================================================================================================
-  Basically, all of files downloaded from my website can be modified or redistributed for any purpose.
-  It is my honor to share my interesting to everybody.
-  If you find any illeage content out from my website, please contact me firstly.
-  I will remove all of the illeage parts.
-  Thanks :)
-   
-  Steward Fu
-  g9313716@yuntech.edu.tw
-  https://steward-fu.github.io/website/index.htm
-========================================================================================================*/
 #include <wdm.h>
 
-#define IOCTL_QUEUE_IT    CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
-#define IOCTL_PROCESS_IT  CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_NEITHER, FILE_ANY_ACCESS)
+#define IOCTL_QUEUE   CTL_CODE(FILE_DEVICE_UNKNOWN, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
+#define IOCTL_PROCESS CTL_CODE(FILE_DEVICE_UNKNOWN, 0x801, METHOD_NEITHER, FILE_ANY_ACCESS)
 
-#define DEV_NAME  L"\\Device\\firstQueue"
-#define SYM_NAME  L"\\DosDevices\\firstQueue"
-#define MSG       "WDM driver tutorial for Queue"
+#define DEV_NAME L"\\Device\\MyDriver"
+#define SYM_NAME L"\\DosDevices\\MyDriver"
 
 PDEVICE_OBJECT gNextDevice=NULL;
 LIST_ENTRY stQueue={0};
 KDPC stTimeDPC={0};
 KTIMER stTime={0};
 
-//*** timer routine
 VOID OnTimer(struct _KDPC *Dpc, PVOID DeferredContext, PVOID SystemArgument1, PVOID SystemArgument2)
 {
   PIRP pIrp;
   PLIST_ENTRY plist;
   if(IsListEmpty(&stQueue) == TRUE){
     KeCancelTimer(&stTime);
-    DbgPrint("OnTimer: Process successfully");
+    DbgPrint("Finish");
   }
   else{
     plist = RemoveHeadList(&stQueue);
@@ -38,18 +25,16 @@ VOID OnTimer(struct _KDPC *Dpc, PVOID DeferredContext, PVOID SystemArgument1, PV
     pIrp->IoStatus.Status = STATUS_SUCCESS;
     pIrp->IoStatus.Information = 0;
     IoCompleteRequest(pIrp, IO_NO_INCREMENT);
-    DbgPrint("OnTimer: Processing Irp");
+    DbgPrint("Complete Irp");
   }
 }
 
-//*** system will vist this routine when it needs to add new device
 NTSTATUS AddDevice(PDRIVER_OBJECT pOurDriver, PDEVICE_OBJECT pPhyDevice)
 {
   PDEVICE_OBJECT pOurDevice=NULL;
   UNICODE_STRING usDeviceName;
   UNICODE_STRING usSymboName;
 
-  DbgPrint(MSG);
   RtlInitUnicodeString(&usDeviceName, DEV_NAME);
   IoCreateDevice(pOurDriver, 0, &usDeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &pOurDevice);
   RtlInitUnicodeString(&usSymboName, SYM_NAME);
@@ -63,14 +48,12 @@ NTSTATUS AddDevice(PDRIVER_OBJECT pOurDriver, PDEVICE_OBJECT pPhyDevice)
   KeInitializeDpc(&stTimeDPC, OnTimer, pOurDevice);
   return STATUS_SUCCESS;
 }
-  
-//*** it is time to unload our driver
+
 void Unload(PDRIVER_OBJECT pOurDriver)
 {
   pOurDriver = pOurDriver;
 }
 
-//*** process pnp irp
 NTSTATUS IrpPnp(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
 {
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
@@ -86,23 +69,22 @@ NTSTATUS IrpPnp(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
   return IoCallDriver(gNextDevice, pIrp);
 }
 
-//*** process ioctl irp
 NTSTATUS IrpIOCTL(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
 {
   LARGE_INTEGER stTimePeriod;
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
 
   switch(psk->Parameters.DeviceIoControl.IoControlCode){
-  case IOCTL_QUEUE_IT:
-    DbgPrint("IrpIOCTL, IOCTL_QUEUE_IT\n");
+  case IOCTL_QUEUE:
+    DbgPrint("IOCTL_QUEUE");
     InsertHeadList(&stQueue, &pIrp->Tail.Overlay.ListEntry);
     IoMarkIrpPending(pIrp);
     return STATUS_PENDING;
-  case IOCTL_PROCESS_IT:
-    DbgPrint("IrpIOCTL, IOCTL_PROCESS_IT\n");
+  case IOCTL_PROCESS:
+    DbgPrint("IOCTL_PROCESS");
     stTimePeriod.HighPart|= -1;
-    stTimePeriod.LowPart = -1000000;
-    KeSetTimerEx(&stTime, stTimePeriod, 1000, &stTimeDPC);
+    stTimePeriod.LowPart = -10000000;
+    KeSetTimerEx(&stTime, stTimePeriod, 10, &stTimeDPC);
     break;
   }
   pIrp->IoStatus.Information = 0;
@@ -111,24 +93,22 @@ NTSTATUS IrpIOCTL(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
   return STATUS_SUCCESS;
 }
 
-//*** process file irp
 NTSTATUS IrpFile(PDEVICE_OBJECT pOurDevice, PIRP pIrp)
 {
   PIO_STACK_LOCATION psk = IoGetCurrentIrpStackLocation(pIrp);
 
   switch(psk->MajorFunction){
   case IRP_MJ_CREATE:
-    DbgPrint("IrpFile, IRP_MJ_CREATE\n");
+    DbgPrint("IRP_MJ_CREATE");
     break;
   case IRP_MJ_CLOSE:
-    DbgPrint("IrpFile, IRP_MJ_CLOSE\n");
+    DbgPrint("IRP_MJ_CLOSE");
     break;
   }
   IoCompleteRequest(pIrp, IO_NO_INCREMENT);
   return STATUS_SUCCESS;
 }
-  
-//*** driver entry
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT pOurDriver, PUNICODE_STRING pOurRegistry)
 {
   pOurDriver->MajorFunction[IRP_MJ_PNP] = IrpPnp;
